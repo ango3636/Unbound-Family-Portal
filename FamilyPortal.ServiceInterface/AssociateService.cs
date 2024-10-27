31429446096b7ac0a10271e4fc52ca5a8266653c
ango3636 <ango3636@gmail.com>
2024-10-27 18:10:01 -0500

    associate service for account management

---
+++ FamilyPortal.ServiceInterface/AssociateService.cs
@@ -0,0 +1,38 @@
+using FamilyPortal.Data;
+using FamilyPortal.ServiceModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace FamilyPortal.ServiceInterface
+{
+    public class AssociateService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AssociateService(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public async Task<Associate> GetAssociateByUserIdAsync(string userId)
+        {
+            return await _context.Associate
+                                .Where(a => a.UserId == userId)
+                                .FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateAssociateAsync(Associate associate)
+        {
+            var existingAssociate = await _context.Associate.FindAsync(associate.AssociateID);
+            if (existingAssociate != null)
+            {
+                existingAssociate.FirstName = associate.FirstName;
+                existingAssociate.LastName = associate.LastName;
+                existingAssociate.PhoneNumber = associate.PhoneNumber;
+
+                _context.Entry(associate).State = EntityState.Modified;
+                _context.Associate.Update(existingAssociate);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- FamilyPortal.ServiceInterface/Data/ApplicationDbContext.cs
+++ FamilyPortal.ServiceInterface/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using FamilyPortal.ServiceModel;
 
 namespace FamilyPortal.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<Associate> Associate { get; set; }
 }
